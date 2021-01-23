67864920b532f55a79fb5f2e9ed5c0b3bfba472c
Milena Ferraz <milenaferraz@gmail.com>
2021-01-22 21:45:32 -0300

    instalação das libs e rotas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'animate.css'
 
-createApp(App).use(router).mount('#app')
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
